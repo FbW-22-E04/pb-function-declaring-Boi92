8d0ecc4e3a8e7d0ee316aa7c5bd95eb279c05b0b
Boi Schrader <boi1992@t-online.de>
2022-07-13 14:45:36 +0200

    task complete

---
+++ solution.js
@@ -0,0 +1,38 @@
+//1.
+function multiply(num1, num2){
+    console.log("1.",num1 * num2);
+}
+
+multiply(2,2);
+//2.
+let multiply2 = function(num1, num2)
+{
+    console.log("2.",num1 * num2);
+}
+multiply2(3,2);
+//3.
+let multiply3 = (num1,num2) =>
+{
+    console.log("3.", num1 * num2);
+}
+multiply3(4,5)
+
+//4.
+function remainder (num1,num2)
+{
+    console.log("4.1",num1 % num2);
+}
+remainder(12,5)
+
+const remainder2 = function (num1,num2)
+{
+    console.log("4.2",num1 % num2)
+}
+remainder2(24,10)
+
+let remainder3 = (num1,num2) =>
+{
+    console.log("4.3", num1 % num2);
+}
+remainder3(20,6)
+
